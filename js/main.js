5b4b805615d1e00caf11be62dc7fcecb60c5e187
ki33520 <ki33520@gmail.com>
2016-10-12 18:12:29 +0800

    fix

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ var indexUrl = 'http://hide.dzhcn.cn/honda/',
 	ajaxUrl = indexUrl+'callback.php',
 	boardType = 'Leaderboard2';
 
-var startTime = new Date().getTime();
+var startTime;
 function getTen(start){
 	var activeTime = new Date().getTime();
 	var times = activeTime - startTime;
@@ -17,9 +17,15 @@ function getTen(start){
 	setTimeout(function(){
 		getTen()
 	},1000)
-}		
+}
+var boardList = $('.board-list');
+boardList.each(function(){
+	for(var i=0;i<10;i++){
+		var li = $("<li></li>");
+		li.appendTo(boardList);
+	}
+});
 function refreshTen(){
-	var boardList = $('.board-list');
 	$.ajax({
 		url: ajaxUrl,
 		type: "post",
@@ -31,15 +37,14 @@ function refreshTen(){
 		success: function(data){
 			if(data.status === 1){
 				var list_data = data.data;
-				boardList.empty();
 				$(list_data).each(function(index,item){
 					var list_node = _.find(window.worksList, function(node){ return node.name == item.Name; });
 					var img = list_node && list_node.img ? list_node.img : '';
-					var li = $('<li><div class="title">第'+item.rowno+'名</div><div class="img"><div class="img-cover"><img src="images/img_cover.png" /></div><div class="img-wrap"><img src='+img+' /></div></div><div class="cf dis"><div class="left">得票率：'+item.vote+'</div><div class="right">'+item.Name+'车队</div></div></li>');
-					setTimeout(function(){
-						li.appendTo(boardList);
-					},100*index);
-					
+					var li_inner = $('<div class="inner"><div class="title">第'+item.rowno+'名</div><div class="img"><div class="img-cover"><img src="images/img_cover.png" /></div><div class="img-wrap"><img src='+img+' /></div></div><div class="cf dis"><div class="left">得票率：'+item.vote+'</div><div class="right">'+item.Name+'车队</div></div></div>');
+					boardList.find('li').eq(index).html(li_inner).addClass('cardLeft animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+						$(this).removeClass('cardLeft animated');
+					});
+
 				});
 			}else{
 				console.log(data)
@@ -47,7 +52,7 @@ function refreshTen(){
 		}
 	});
 }
-getTen(true);
+
 
 $('body.ex').on('click',function(){
 	$('.board').each(function(index,item){
@@ -55,6 +60,10 @@ $('body.ex').on('click',function(){
 			$(item).removeClass('show');
 		}else{
 			$(item).addClass('show');
+			if(index == 1){
+				startTime = new Date().getTime();
+				getTen(true);
+			}
 		}
 	})
 });
